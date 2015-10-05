4bc8df04aa9f306f98e7a96909997eefa10d4712
Lionel Koenig <lionelk@microsoft.com>
2015-10-05 14:57:12 +0200

    Hum ...

--- navsim.py
+++ navsim.py
@@ -1,26 +1,36 @@
 
 
 class Position(object):
-    def __init__(self):
-        pass
-        
+    """
+    Position in North - East - Down coordinates
+    """
+    def __init__(self, latitude, longitude, down=0):
+        self.lat = latitude
+        self.long = longitude
+        self.down = down
 
 class GameEntity(object):
 
     def __init__(self):
-        self._momentum = ()
-
-    def update(time_delta):
         pass
+
+    def update(self, time_delta):
+        raise Exception("Do not know how to simulate the physics")
     
     
-class Ship(GameEntity):
-    pass
+class Vessel(GameEntity):
+    def __init__(self):
+        self.position = Position(0, 0, 0)
+
+    def update(self, time_delta):
+        pass
     
     
 class Game(object):
     def __init__(self):
         self._entities = list()
+
+        self._add_game_object()
     
     def main_loop(self):
         
